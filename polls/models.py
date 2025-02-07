bcdf718d5297ea8721683b3c6bf3f19bd1000684
Sai Varala <sai.varala@outlook.com>
2025-02-06 23:43:48 -0500

    changes by sai

--- polls/models.py
+++ polls/models.py
@@ -1,25 +1,29 @@
-from django.db import models
-
-# Create your models here.
 import datetime
-from django.db import models
+
 from django.db import models
 from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)  # A short string field
-    pub_date = models.DateTimeField("date published")  # A datetime field
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # Foreign key to Question
-    choice_text = models.CharField(max_length=200)  # A short string field
-    votes = models.IntegerField(default=0)  # Integer field with default value 0
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
